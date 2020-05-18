f2c524d0de5aa0e52e5fb97e180be534ea390d69
trental <trentlehmann@gmail.com>
2020-05-18 11:23:06 -0500

    Link table established through item and need creation

---
+++ controllers/needController.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const Need = require('../models/Need');
+
+router.get('/', (req, res) => {
+	Need.find().then((allNeeds) => {
+		res.json(allNeeds);
+	});
+});
+
+router.delete('/:id', (req, res, next) => {
+	Need.findOneAndDelete({ _id: req.params.id })
+		.then((needs) => {
+			res.json(needs);
+		})
+		.catch(next);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -7,17 +7,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const userController = require('./controllers/userController')
+const userController = require('./controllers/userController');
 app.use('/api/user', userController);
 
-const categoryController = require('./controllers/categoryController')
+const categoryController = require('./controllers/categoryController');
 app.use('/api/category', categoryController);
 
-const itemController = require('./controllers/itemController')
-app.use('/api/item', itemController)
+const itemController = require('./controllers/itemController');
+app.use('/api/item', itemController);
 
-const tierController = require('./controllers/tierController')
-app.use('/api/tier', tierController)
+const needController = require('./controllers/needController');
+app.use('/api/need', needController);
+
+const tierController = require('./controllers/tierController');
+app.use('/api/tier', tierController);
 
 app.set('port', process.env.PORT || 8080);
 
